9f0c7637d7c55a13c26c412fb33866dc3f9a1f98
btd-hub <abeey4u@gmail.com>
2023-06-25 15:02:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("navOpen").addEventListener("click", toggleNav);
+function toggleNav() {
+document.getElementById("navbar-sticky").classList.toggle("hidden");
+document.getElementById("navbar-sticky").classList.toggle("show");
+}
+
+document.getElementById("country").addEventListener("click", function () {
+    document.getElementById("")
+    
+})
+
+var widget =  document.getElementById("horizon");
+var interval;
+
+
+
+
